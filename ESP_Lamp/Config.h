a7bb0885eb4c7a5e9ecbcd9b201e9778a15bd832
polina <117908658+PolinaOzhigova@users.noreply.github.com>
2023-10-23 23:54:03 +0800

    add ESP_lamp

---
+++ ESP_Lamp/Config.h
@@ -0,0 +1,13 @@
+String AP_NAME = "PolinaOzh"; // AP WIFI access point
+String AP_PASSWORD = "12345678";
+
+// CLI WIFI client
+const char* CLI_SSID = "Poli_O"; // turn phone AP 5 GHz -> 2.4 GHz
+const char* CLI_PASS = "00001111";
+
+int WEB_SERVER_PORT = 80;
+
+const char* mqtt_broker = "broker.emqx.io";
+const int mqtt_port = 1883;
+
+bool AP_mode = true;
