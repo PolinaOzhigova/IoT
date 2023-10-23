cdb4c60fb6c467627b25bc72c4b464af5062c5cc
polina <117908658+PolinaOzhigova@users.noreply.github.com>
2023-10-24 00:00:12 +0800

    ADD pub.py work

---
+++ ESP_Lamp/pub.py
@@ -0,0 +1,30 @@
+import paho.mqtt.client as mqtt_client 
+import random 
+import time 
+ 
+broker = "broker.emqx.io" 
+min_brightness_time = 10   
+def on_connect(client, userdata, flags, rc): 
+    if rc == 0: 
+        print("Connected to MQTT Broker!") 
+    else: 
+        print("Failed to connect, return code %d\n", rc) 
+ 
+client = mqtt_client.Client(f'lab_{random.randint(10000, 99999)}') 
+client.on_connect = on_connect 
+client.connect(broker) 
+ 
+brightness_time = 20 
+ 
+while True: 
+    state = "d" 
+    client.publish("esp8266f32a/command", state) 
+    print(f"publish state is {state}") 
+    time.sleep(brightness_time) 
+ 
+    brightness_time = max(min_brightness_time, brightness_time - 1) 
+ 
+    state = "u" 
+    client.publish("esp8266f32a/command", state) 
+    print(f"publish state is {state}") 
+    time.sleep(60 - brightness_time)
