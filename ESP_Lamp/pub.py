054d519b722a7092bbb75a8668fcbedcb8ad76ad
polina <117908658+PolinaOzhigova@users.noreply.github.com>
2023-12-25 21:55:07 +0800

    fix time to utc and correct work

--- ESP_Lamp/pub.py
+++ ESP_Lamp/pub.py
@@ -1,30 +1,38 @@
 import paho.mqtt.client as mqtt_client 
 import random 
-import time 
+import time
+from datetime import datetime, timedelta
  
-broker = "broker.emqx.io" 
-min_brightness_time = 10   
-def on_connect(client, userdata, flags, rc): 
-    if rc == 0: 
+broker = "broker.emqx.io"
+def on_connect(client, userdata, flags, recieve_c): 
+    if recieve_c == 0: 
         print("Connected to MQTT Broker!") 
     else: 
-        print("Failed to connect, return code %d\n", rc) 
+        print("Failed to connect, return code %d\n", recieve_c) 
  
 client = mqtt_client.Client(f'lab_{random.randint(10000, 99999)}') 
 client.on_connect = on_connect 
 client.connect(broker) 
- 
-brightness_time = 20 
- 
-while True: 
-    state = "d" 
-    client.publish("esp8266f32a/command", state) 
-    print(f"publish state is {state}") 
-    time.sleep(brightness_time) 
- 
-    brightness_time = max(min_brightness_time, brightness_time - 1) 
- 
-    state = "u" 
-    client.publish("esp8266f32a/command", state) 
-    print(f"publish state is {state}") 
-    time.sleep(60 - brightness_time)
+
+min_time = 10
+start_time = 20
+
+while True:
+    state = "d"
+    client.publish("esp8266f32a/command", state)
+    print(f"publish state is {state}")
+
+    time.sleep(start_time)
+
+    start_time = max(min_time, start_time - 1)
+
+    state = "u"
+    client.publish("esp8266f32a/command", state)
+    print(f"publish state is {state}")
+
+    current_time = datetime.utcnow()
+    next_minute = (current_time + timedelta(minutes=1)).replace(second=0, microsecond=0)
+    time_until_next_minute = (next_minute - current_time).total_seconds()
+    sleep_time = time_until_next_minute - start_time
+
+    time.sleep(sleep_time)
