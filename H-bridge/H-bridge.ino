85771e9605699aa492b16d1bbe8f50f6093880df
polina <117908658+PolinaOzhigova@users.noreply.github.com>
2024-05-24 15:05:46 +0800

    oh, I've forgotten about function

--- H-bridge/H-bridge.ino
+++ H-bridge/H-bridge.ino
@@ -1,3 +1,4 @@
+
 #define PIN_L 3
 #define PIN_R 5
 
@@ -5,22 +6,40 @@ void setup(){
   pinMode(PIN_L, OUTPUT);
   pinMode(PIN_R, OUTPUT);
   Serial.begin(9600);
+  
+  Serial.println("Input l or r and then speed. Ex: l40");
 }
 
 void loop(){
   if (Serial.available()) {
     String input = Serial.readString();
+    Serial.println(input);
     char command = input[0];
     input.remove(0, 1); 
     int speed = input.toInt();
-    if (command == 'l') {
-        analogWrite(PIN_L, speed); 
-    	analogWrite(PIN_R, 0);
-    } else if (command == 'r') {
-        analogWrite(PIN_R, speed); 
-    	analogWrite(PIN_L, 0);
-    } else {
-      Serial.print("Unknown command, input l or r ");
-    }
+    
+    moveMotor(command, speed);
+  }
+}
+
+void moveMotor(char dir, int speed){
+  
+  if (speed > 255){
+    speed = 255;
+  }else if (speed < 0) {
+    speed = 0;
+  }
+  
+  if (dir == 'l') {
+  	analogWrite(PIN_L, speed); 
+  	analogWrite(PIN_R, 0);
+  	return;
+  } else if (dir == 'r') {
+  	analogWrite(PIN_R, speed); 
+  	analogWrite(PIN_L, 0);
+  	return;
+  } else {
+  	Serial.println("Unknown command, input l or r ");
+   	return;
   }
 }
